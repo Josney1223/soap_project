ccba5a7e4f4b6f27771dcb9c6ff9d4acce41e467
Leonardo Lopes Lessa da Fonseca <leonardo.lessa@materacapital.com.br>
2022-11-20 21:43:44 -0300

    Commit

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import zeep
 hello_client = zeep.Client('http://localhost:8000/?wsdl')
 print(hello_client.service.fazer_pedido("Leonardo", "Combinado 2"))
 print(hello_client.service.fazer_pedido("Lorenza", "Barca para 2"))
+print(hello_client.service.fazer_pedido("Kahao", "Drink Namorada"))
 print(hello_client.service.fazer_pedido("Pedro", "Coca"))
 print(hello_client.service.ver_pedidos())
 print(hello_client.service.atualiza_pedido(1, "Cancelado"))
